c6ed915fd57bad9a25076bb68340f66f421d1354
zpqwe <zpadmin123456>
2021-01-08 09:15:12 +0800

    有效的字母异位词 验证回文串

---
+++ src/com/zp/solution/Soulution10.java
@@ -0,0 +1,24 @@
+package com.zp.solution;
+
+/**
+ * @program: mydemo
+ * @author: Mr.zeng
+ * @create: 2021-01-07 09:17
+ *
+ * 验证回文串
+ **/
+public class Soulution10 {
+    public boolean isPalindrome(String s){
+        StringBuffer sgood=new StringBuffer();
+        int length=s.length();
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if(Character.isLetterOrDigit(c)){
+                sgood.append(Character.toLowerCase(c));
+            }
+        }
+        StringBuffer sgood_rev=new StringBuffer(sgood).reverse();
+        return sgood.toString().equals(sgood_rev.toString());
+    }
+
+}
---
+++ src/com/zp/solution/Soulution9.java
@@ -0,0 +1,24 @@
+package com.zp.solution;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @program: mydemo
+ * @author: Mr.zeng
+ * @create: 2021-01-06 09:26
+ *
+ * 有效的字母异位词
+ **/
+public class Soulution9 {
+    public boolean isAnagram(String s,String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        char[] charst = t.toCharArray();
+        Arrays.sort(chars);
+        Arrays.sort(charst);
+        return Arrays.equals(chars,charst);
+    }
+}
