c68b1f95bd0d21d89dbacd771eb0bb969e8aa736
zpqwe <zpadmin123456>
2021-02-01 09:18:36 +0800

    对称二叉树

---
+++ src/com/zp/solution/Solution22.java
@@ -0,0 +1,45 @@
+package com.zp.solution;
+
+/**
+ * @program: mydemo
+ * @author: Mr.zeng
+ * @create: 2021-02-01 09:09
+ *
+ * 对称二叉树
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ *
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ **/
+public class Solution22 {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public boolean isSymmetric(TreeNode root) {
+        return check(root,root);
+    }
+    public boolean check(TreeNode p,TreeNode q){
+        if(p==null&&q==null){
+            return true;
+        }
+        if(p==null||q==null){
+            return false;
+        }
+        return p.val==q.val&&check(p.left,q.right)&&check(p.right,q.left);
+    }
+}
